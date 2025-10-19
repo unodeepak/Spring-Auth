c75879ba9cc4f4ed59e2a235c75e948bca1d091c
Deepak <deepakkeshri@tataagi.com>
2025-10-19 21:14:34 +0530

    Init the app

---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,20 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String first_name;
+    private String last_name;
+    private int age;
+    private String gender;
+}
